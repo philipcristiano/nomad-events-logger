54869fe94120f88bab35f6d853928b2ffac9078f
Philip Cristiano <git@philipcristiano.com>
2023-05-27 10:50:55 -0400

    Start project

---
+++ shell.nix
@@ -0,0 +1,26 @@
+let
+  sysPkg = import <nixpkgs> { };
+  releasedPkgs = sysPkg.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "22.11";
+    sha256 = "sha256-/HEZNyGbnQecrgJnfE8d0WC5c1xuPSD2LUpB6YXlg4c=";
+  };
+  pkgs = import releasedPkgs {};
+  stdenv = pkgs.stdenv;
+  extraInputs = sysPkg.lib.optionals stdenv.isDarwin (with sysPkg.darwin.apple_sdk.frameworks; [
+    Cocoa
+    CoreServices]);
+
+in stdenv.mkDerivation {
+  name = "env";
+  buildInputs = [ pkgs.gnumake
+                  pkgs.wget
+                  pkgs.cargo
+                  pkgs.rustc
+
+                ] ++ extraInputs;
+  shellHook = ''
+
+  '';
+}
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use serde_json::{Value};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Nomad API
+    #[arg(short, long, default_value_t = String::from("http://127.0.0.1:4646/v1/event/stream"))]
+    url: String,
+
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    let url = args.url;
+    let mut res = reqwest::get(url).await?;
+
+    // loop until \n encountered, adding chunks to a string or something, then try to parse
+    loop {
+        let mut chunks = vec!();
+        while let Some(chunk) = res.chunk().await? {
+            let is_newline = chunk.eq("\n");
+            chunks.push(chunk);
+            if is_newline {break};
+        }
+        let v: Value = serde_json::from_slice(chunks.concat().as_slice())?;
+        println!("Chunk: {:?}", v);
+    }
+
+    Ok(())
+}
