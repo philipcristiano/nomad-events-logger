698e7c4b4ca45af52d4e2c24b062314ea4000e7d
Philip Cristiano <git@philipcristiano.com>
2023-06-04 17:18:27 -0400

    nix: Upgrade nix release

--- shell.nix
+++ shell.nix
@@ -3,8 +3,8 @@ let
   releasedPkgs = sysPkg.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "22.11";
-    sha256 = "sha256-/HEZNyGbnQecrgJnfE8d0WC5c1xuPSD2LUpB6YXlg4c=";
+    rev = "23.05";
+    sha256 = "sha256-btHN1czJ6rzteeCuE/PNrdssqYD2nIA4w48miQAFloM=";
   };
   pkgs = import releasedPkgs {};
   stdenv = pkgs.stdenv;
@@ -18,6 +18,8 @@ in stdenv.mkDerivation {
                   pkgs.wget
                   pkgs.cargo
                   pkgs.rustc
+                  pkgs.libiconv # https://stackoverflow.com/questions/68679040/error-linking-with-cc-failed-exit-code-1-for-cargo-run
+                  pkgs.rust-analyzer
 
                 ] ++ extraInputs;
   shellHook = ''
